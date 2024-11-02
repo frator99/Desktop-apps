30715e8d677604c7e78676c7874b4ddf0106309f
francesco <francescotorraco@gmail.com>
2024-11-02 13:26:01 +0100

    aggiunte ezceptions, aggiunto un orologio e cambiato il tema

--- gui-todo.py
+++ gui-todo.py
@@ -1,8 +1,12 @@
 import FreeSimpleGUI as sg
 import functions_todo
+import time
+#qui possimao definire il tema
+sg.theme("BlueMono")
+
 
 label = sg.Text("inserisci un azione nella to-do list")
-
+label_clock = sg.Text("", key='clock')
 inputbox = sg.InputText(tooltip="inserisci un' azione", key="todo")
 button = sg.Button("aggiungi")
 # il valore del layout dell'oggetto "Window" richiede un' array di array si oggetti  , quindi ci vanno diverse
@@ -19,7 +23,7 @@ button_list = sg.Button("edit")
 
 button_complete = sg.Button("complete")
 button_exit = sg.Button("exit")
-window = sg.Window("la mia to-do app", layout=[[label],
+window = sg.Window("la mia to-do app", layout=[[label, label_clock],
                                                [inputbox, button],
                                                [listbox, button_list , button_complete],
                                                [button_exit]], font="Helvetica")
@@ -33,9 +37,10 @@ window = sg.Window("la mia to-do app", layout=[[label],
 
 #oppure valorizzare correttamente con 2 variabili separate
 while True:
-    event ,values = window.read()
-    print(event)
-    print(values)
+    event ,values = window.read(timeout="10") # aggiungiamo un timeout di 10 millisecondi per permettere di ciclare
+    #l'update della label_clock
+
+    window['clock'].update(value=time.strftime("%b %d, %Y %H: %M: %S"))
 
     match event:
         case "aggiungi":
@@ -49,41 +54,43 @@ while True:
             #questa variabile della libreria è valorizzata a TRUE solo quando premiamo tale bottone.
             break
         case "edit":
-            todo_to_edit = values["todos"] [0]
-            #todo è la chiave della prima inputbox, quindi scegliamo un valore nella listbox e lo sost.
-            #con quello nella inputbox
-            new_todo = values["todo"] + "\n"
-            todos = functions_todo.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions_todo.write_todos(todos)
-            #facciamo l'update della lista in gui a real-time , aggiornando il valore dell'oggetto windows
-            window['todos'].update(values=todos)
-            #con window[todos]indichiamo l'istanza della windows che ha come
-            #chiave il valore "todos" , nel nostro caso è la listbox a riga 17.
+            #gestiamo l'errore del click su edit senza aver insertito nulla nella box
+            try:
+                todo_to_edit = values["todos"] [0]
+                #todo è la chiave della prima inputbox, quindi scegliamo un valore nella listbox e lo sost.
+                #con quello nella inputbox
+                new_todo = values["todo"] + "\n"
+                todos = functions_todo.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions_todo.write_todos(todos)
+                #facciamo l'update della lista in gui a real-time , aggiornando il valore dell'oggetto windows
+                window['todos'].update(values=todos)
+                #con window[todos]indichiamo l'istanza della windows che ha come
+                #chiave il valore "todos" , nel nostro caso è la listbox a riga 17.
+            except IndexError:
+                sg.popup("seleziona un valore prima di premere edit!", font=("Helvetica", "15"))
 
         case "todos":
-            # qui aggiorniamo il campo della inputbox quando clicchiamo un items della list , in modo che
+            # qui aggiorniamo il campo della inputbox quando clicchiamo edit senza selez. nulla
             #compaia il testo selezionato
             window['todo'].update(value=values['todos'][0])
         case "complete":
-            todo_to_complete = values["todos"][0]
-            todos = functions_todo.get_todos()
-            #.index rest. indice dell'item
-            index = todos.index(todo_to_complete)
-            print("index = ", index, "che equivale a :", todos[index])
-            todos.pop(index)
-            functions_todo.write_todos(todos)
-            window["todos"].update(values=todos)
-            window["todo"].update(value='')
+            #gestiamo errore valore non selezionato
+            try:
+                todo_to_complete = values["todos"][0]
+                todos = functions_todo.get_todos()
+                #.index rest. indice dell'item
+                index = todos.index(todo_to_complete)
+                print("index = ", index, "che equivale a :", todos[index])
+                todos.pop(index)
+                functions_todo.write_todos(todos)
+                window["todos"].update(values=todos)
+                window["todo"].update(value='')
+            except IndexError:
+                sg.popup("seleziona un valore prima di premere complete!", font=("Helvetica", "15"))
         case "exit":
             break
-
-
-
-
-
-
 
 #una volta usciti dal ciclo potremo chiudere la finestra con window.close.
 window.close()
